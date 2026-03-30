af7725667816be505b5662e1987709641bc7f265
ikromr04 <ikromr04@gmail.com>
2026-03-30 15:36:18 +0500

    fix(mode-toggle): use button toggle instead of dropdown

--- src/pages/home/ui/home-page.tsx
+++ src/pages/home/ui/home-page.tsx
@@ -18,12 +18,10 @@ function HomePage(): JSX.Element {
   return (
     <div className="flex flex-1 flex-col">
       <div className="@container/main flex flex-1 flex-col gap-2">
-        <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
+        <div className="flex flex-col gap-4 md:gap-6">
           <SectionCards />
-          <div className="px-4 lg:px-6">
-            <ChartAreaInteractive />
-          </div>
-          <div className="grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-linear-to-t *:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4 dark:*:data-[slot=card]:bg-card">
+          <ChartAreaInteractive />
+          <div className="grid grid-cols-1 gap-4 *:data-[slot=card]:bg-linear-to-t *:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card *:data-[slot=card]:shadow-xs @xl/main:grid-cols-2 @5xl/main:grid-cols-4 dark:*:data-[slot=card]:bg-card">
             <ChartPieLabel />
             <ChartRadarDots />
             <div className="col-span-2">
--- src/widgets/app-layout/ui/app-header.tsx
+++ src/widgets/app-layout/ui/app-header.tsx
@@ -7,11 +7,11 @@ function AppHeader(): JSX.Element {
   const { title } = useHeader();
 
   return (
-    <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
-      <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
+    <header className="sticky top-0 z-10 flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
+      <div className="flex w-full h-full bg-background items-center gap-1 mxx-4 lg:gap-2 lg:mx-6">
         <SidebarTrigger className="-ml-1" />
 
-        <Separator className="mx-2 data-[orientation=vertical]:h-4" orientation="vertical" />
+        <Separator className="mx-2 my-auto data-[orientation=vertical]:h-4" orientation="vertical" />
 
         <h1 className="text-base font-medium">{title}</h1>
 
